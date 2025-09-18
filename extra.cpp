c9ab4d85dbc3ff3fec80160e57127c5adaabbbb6
Илья Солопов <solopov@sfedu.ru>
2025-09-18 16:44:28 +0300

    fix: Исправленны ошибк в extra.cpp

--- extra.cpp
+++ extra.cpp
@@ -1,10 +1,8 @@
-#include "./main.cpp"
-
-int myPow(int x, int n = 2){
+static int myPow(int x, int n = 2) {
 
     int res = x;
-    for(int i = 1; i < n; ++i){
-	res *= x;
+    for (int i = 1; i < n; ++i) {
+        res *= x;
     }
 
     return res;
